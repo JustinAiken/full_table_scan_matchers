2d8db47f804f7ed76b3c6e09c5cf9d059226e72a
Justin Aiken <60tonangel@gmail.com>
2016-09-26 12:15:29 -0600

    Flesh out config wrapper specs

--- spec/lib/full_table_scan_matchers_spec.rb
+++ spec/lib/full_table_scan_matchers_spec.rb
@@ -6,4 +6,18 @@ describe FullTableScanMatchers do
       expect(FullTableScanMatchers.configuration).to be_a FullTableScanMatchers::Configuration
     end
   end
+
+  describe ".configure / .reset_configuration" do
+    before do
+      FullTableScanMatchers.configure do |config|
+        config.ignores = [:foo]
+      end
+    end
+
+    it "sets and resets the config" do
+      expect { FullTableScanMatchers.reset_configuration}.to change {
+        FullTableScanMatchers.configuration.ignores
+      }.from([:foo]).to []
+    end
+  end
 end
